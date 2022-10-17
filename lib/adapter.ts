1268dd8e4619c570ab9e5ea922221499f1ebaed1
breadfish_18 <42822038+breadfish18@users.noreply.github.com>
2022-10-17 16:45:36 -0600

    fix

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-export function getBackpackFromAPI(
-    steamid,
-    appid,
-    axiosInstance = axios.create()
-) {
+export function getBackpackFromAPI(steamid, appid, axiosInstance?) {
+    if (!axiosInstance) axiosInstance = axios.create();
     let url = `http://steamcommunity.com/inventory/${steamid}/${appid}/2?l=english&count=5000`;
     return axiosInstance.get(url).then(({ data }) => data);
 }
